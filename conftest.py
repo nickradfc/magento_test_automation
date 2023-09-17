9a6584cf2e5498a279925aef3f55c6d88e4ff505
Nik Radililovsky <nik@Niks-MacBook.local>
2023-09-16 19:29:22 -0700

    cleanup

--- conftest.py
+++ conftest.py
@@ -1,9 +1,1 @@
-# # In your conftest.py
-# import pytest
-# from utilities.driver_manager import get_chrome_driver, get_firefox_driver
-# from utilities.env_reader import read_env_variable
-# from utilities.general_utils import generate_random_email
-
-# # Further down in the file where you set up the webdriver
-# def setup_method(self):
-#     self.driver = get_chrome_driver()  # or get_firefox_driver() depending on which one you want to use
+# In your conftest.py
